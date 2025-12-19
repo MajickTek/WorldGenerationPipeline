ccb39410b86996b3771ebfcf1a7bd14dfe08be11
Majick Tek <kingtmw2@gmail.com>
2025-12-18 20:36:20 -0500

    Fix progress update message formatting

--- src/com/mt/worldgen/pipeline/test/ConsoleProgessMonitor.java
+++ src/com/mt/worldgen/pipeline/test/ConsoleProgessMonitor.java
@@ -1,7 +1,9 @@
+import java.text.MessageFormat;
 public class ConsoleProgressMonitor implements ProgressListener {
   @Override
   public void onProgressUpdate(nt currentStageIndex, int totalStages, String stageName, String status) {
     double percentage = (double) currentStageIndex / totalStages * 100;
-    System.out.println("\r[%.1f%%] Step %d/%d: %s", percentage, currentStageIndex, status);
+    String statusMessage = MessageFormat.format("{0}% step {1}/{2}: {3}",percentage,currentStageIndex,status);
+    System.out.println(statusMessage);
   }
 }
