28a1407d4f068aef7c1fa4f9fcbc563b0a038110
Majick Tek <kingtmw2@gmail.com>
2025-12-18 19:59:51 -0500

    Implement ProcessingContext class for pipeline stages

---
+++ src/com/mt/worldgen/pipeline/test/ProcessingContext.java
@@ -0,0 +1,23 @@
+public class ProcessingContext<I,O> {
+  private I input;
+  private O output;
+  private int currentStageIndex = 0;
+  private final int totalStages;
+
+  //other data stored here?
+
+  public ProcessingContext(I input, int totalStages) {
+    this.input = input;
+    this.totalStages = totalStages;
+  }
+
+  public void advanceStage() {
+    this.currentStageIndex++;
+  }
+
+  public int getCurrentProgress() {
+    return (int) ((double) currentStageIndex / totalStages * 100);
+  }
+
+  //maybe add other getters/setters for other included data
+}
