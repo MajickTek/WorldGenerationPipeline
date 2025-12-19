f0348a43b279779aefd08b087754607072a30cb2
Majick Tek <kingtmw2@gmail.com>
2025-12-18 20:37:33 -0500

    Fix parameter type in onProgressUpdate method

--- src/com/mt/worldgen/pipeline/test/ConsoleProgessMonitor.java
+++ src/com/mt/worldgen/pipeline/test/ConsoleProgessMonitor.java
@@ -1,7 +1,7 @@
 import java.text.MessageFormat;
 public class ConsoleProgressMonitor implements ProgressListener {
   @Override
-  public void onProgressUpdate(nt currentStageIndex, int totalStages, String stageName, String status) {
+  public void onProgressUpdate(int currentStageIndex, int totalStages, String stageName, String status) {
     double percentage = (double) currentStageIndex / totalStages * 100;
     String statusMessage = MessageFormat.format("{0}% step {1}/{2}: {3}",percentage,currentStageIndex,status);
     System.out.println(statusMessage);
